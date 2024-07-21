e0caa7746d2923f8d0ae9e4ab204e7a0c1a06d6c
elenavoronin <elnvoronin@gmail.com>
2024-07-21 14:48:43 +0200

    updated

---
+++ ToDoApp/bonus_experiment.py
@@ -0,0 +1,1 @@
+print("The length of the title: ", "\n", len(input("Enter a title: ")))
---
+++ ToDoApp/main.py
@@ -0,0 +1,12 @@
+user_prompt = 'Enter a todo: '
+todo1 = input(user_prompt)
+todo2 = input(user_prompt)
+todo3 = input(user_prompt)
+
+# this is a python list
+
+todos = [todo1, todo2, todo3]
+print(todos)
+
+# shows type of variable`
+print(type(todos))
